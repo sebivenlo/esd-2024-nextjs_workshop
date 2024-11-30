63bd1079c70730277be9ae77c2ed3ae4b50e2ba6
Juliankser <julian.koeser@gmail.com>
2024-11-30 20:18:07 +0100

    added assignemnt 2

---
+++ assignment-2-adding-events/src/app/components/EventCard.tsx
@@ -0,0 +1,22 @@
+// src/app/components/EventCard.tsx
+
+'use client';
+
+import Link from "next/link";
+
+interface EventCardProps {
+  id: string;
+  title: string;
+  date: string;
+}
+
+export default function EventCard({ id, title, date }: EventCardProps) {
+  return (
+    <Link href={`/event/${id}`}>
+      <div className="p-2 bg-blue-100 rounded-md hover:bg-blue-200 transition cursor-pointer">
+        <h3 className="font-semibold">{title}</h3>
+        <p className="text-sm text-gray-600">{date}</p>
+      </div>
+    </Link>
+  );
+}
